1b469324717f8e8c7239f76651b0d9c46d152c72
irenapopova <irenejpopova6@gmail.com>
2020-06-20 00:10:38 +0200

    auth middleware updated

--- server/src/util/auth.js
+++ server/src/util/auth.js
@@ -1,17 +1,19 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+import { User } from "../entity/User";
+import { getRepository } from "typeorm";
+import bcrypt from "bcrypt";
 
-module.exports = new LocalStrategy(
-    (username, password, done) => {
-        if (username === "kantemir" && password === "kantemir") {
-            return done(null, {
-                id: '111351351',
-                username: "kantemir",
-                data: "test"
-            })
-        }
-        return done(null, false, {
-            message: "invalid credentials"
-        })
-    }
-)
+module.exports = new LocalStrategy(async (username, password, done) => {
+  try {
+    const userRepo = getRepository(User);
+    const user = await userRepo.findOne({ username: username });
+    await bcrypt.compare(password, user.password);
+    return done(null, user);
+  } catch (e) {
+    console.log(e);
+    return done(null, false, {
+      message: "invalid credentials",
+    });
+  }
+});
