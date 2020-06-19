f06ba43c2713fa2d5c2f5a0d3482bc4ac3dbabce
irenapopova <irenejpopova6@gmail.com>
2020-06-19 23:56:25 +0200

    migrate to new pc and typeorm clean up

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,57 +1,44 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
-import { User } from "./entity/User";
-import * as bcrypt from 'bcrypt';
 
-
-createConnection().then(async connection => {
-    const saltRounds = 10;
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.username = "Timber";
-    user.password = await bcrypt.hash('kantemir', saltRounds);
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
-
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
-
+createConnection()
+  .then(async (connection) => {
     // setup server once connection to the database is created
-    const express = require('express');
-    const session = require('express-session');
-    const auth = require('./util/auth');
-    const passport = require('passport');
-    const routes = require('./routes/routes');
-    const initSocketServer = require('./socket/socket');
+    const express = require("express");
+    const session = require("express-session");
+    const auth = require("./util/auth");
+    const passport = require("passport");
+    const routes = require("./routes/routes");
+    const initSocketServer = require("./socket/socket");
     const PORT = 5000;
-
 
     const app = express();
 
     // setup express middlewares
-    app.use(session({
+    app.use(
+      session({
         secret: "nyan",
         resave: true,
-        saveUninitialized: true
-    }));
+        saveUninitialized: true,
+      })
+    );
     app.use(passport.initialize());
     app.use(passport.session());
     //setup passport middlewares
-    passport.use(auth)
+    passport.use(auth);
     passport.serializeUser((user, done) => {
-        done(null, user.id);
-    })
+      done(null, user.id);
+    });
     //setup routes
     app.use(routes);
 
     // express server listen on PORT
     const server = app.listen(PORT, (e: Error) => {
-        if (e) return console.log(e)
-        console.log(`server listening on port ${PORT}`)
+      if (e) return console.log(e);
+      console.log(`server listening on port ${PORT}`);
     });
 
     // create websocket server
     const ws = initSocketServer(server);
-
-}).catch(error => console.log(error));
+  })
+  .catch((error) => console.log(error));
