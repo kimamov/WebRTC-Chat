f32f28b2e9ae4d3dbcdde61fe65626e5629b6f1e
kantimam <kantemir.imam@gmail.com>
2020-06-28 21:58:00 +0200

    commit express-ws version

--- server/src/middlewares/middlewares.ts
+++ server/src/middlewares/middlewares.ts
@@ -1,6 +1,8 @@
 // checkAuth middleware checks if the current requests is authenticated
 export function checkAuth(req, res, next) {
-    console.log(req.passport)
+    /* console.log(req)
+    console.log(req.user)
+    console.dir(req.session) */
     if (req.isAuthenticated()) return next();
     next("request not authenticated");
   }
--- server/src/socket/socket.ts
+++ server/src/socket/socket.ts
@@ -1,12 +1,13 @@
 import {checkAuth} from '../middlewares/middlewares'
 
 module.exports= function initSocketServer(app){
-    app.ws('/socket',checkAuth, (ws, req)=>{
-        console.log("WS STUFF")
+    app.ws('/socket', (ws, req)=>{
+        /* console.log("WS STUFF")
         console.dir(req.session)
-        console.dir(req.user)
+        console.dir(req.user) */
         ws.on('message', (message)=>{
             console.log(message)
+            ws.send('got your message')
         })
     })
 }
