4f6d92b6840f9257e05b76b6eea5cad6ca6c9566
Nyannazz <kantemir.imam@gmail.com>
2020-07-04 23:24:54 +0200

    added webrtc logic to accept call

--- client/src/Chat.tsx
+++ client/src/Chat.tsx
@@ -2,23 +2,65 @@ import React, { Component } from 'react'
 import SimplePeer from 'simple-peer'
 import Socket from './components/Socket'
 import { TextField } from '@material-ui/core'
+import { Context } from './state/state';
 
-export interface IAppProps {}
+export interface IAppProps { }
 
 export interface IAppState {
   textInput: string
   data: string
   connected: boolean
+  targetUserId: string
+}
+
+export interface WebSocketMessage {
+  type: string,
+  data: any
+}
+
+function initSocket(username: string, password: string) {
+  if (!username) return null
+  // todo once client and server run on the same oirigin go back to cookie auth
+  const socket = new WebSocket("ws://127.0.0.1:5000?username=" + username + "&password=" + password);
+
+  socket.onopen = function (e) {
+    console.log("[open] Connection established");
+    console.log("Sending to server");
+    socket.send("hey server");
+  };
+
+  socket.onmessage = function (event) {
+    console.log(`[message] Data received from server: ${event.data}`);
+  };
+
+  socket.onclose = function (event) {
+    if (event.wasClean) {
+      console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+    } else {
+      // e.g. server process killed or network down
+      // event.code is usually 1006 in this case
+      console.log('[close] Connection died');
+    }
+  };
+
+  socket.onerror = function (error: any) {
+    console.log(`[error] ${error.message}`);
+  };
+
+  return socket;
 }
 
 export default class Chat extends Component<IAppProps, IAppState> {
+  static contextType = Context;
   peer: any = null
+  private socket: WebSocket | null = null;
   constructor(props: IAppProps) {
     super(props)
     this.state = {
       textInput: '',
       data: '',
       connected: false,
+      targetUserId: ''
     }
   }
   onChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) =>
@@ -28,12 +70,13 @@ export default class Chat extends Component<IAppProps, IAppState> {
     >)
 
   componentDidMount() {
-    this.createPeer()
+    this.socket = initSocket(this.context?.state?.user?.username, "kantemir");
+    this.createPeer(this.context.state.id, "3");
   }
 
-  createPeer = (init = true) => {
+  createPeer = (targetUserId: string, userId: string) => {
     this.peer = new SimplePeer({
-      initiator: init,
+      initiator: true,
       trickle: false,
     })
 
@@ -41,6 +84,14 @@ export default class Chat extends Component<IAppProps, IAppState> {
 
     this.peer.on('signal', (data: any) => {
       console.log('SIGNAL', data)
+      this.socket?.send(JSON.stringify({
+        type: 'offer',
+        data: {
+          target: targetUserId,
+          from: userId,
+          data: data
+        }
+      }))
       this.setState({ data: JSON.stringify(data, null, 2) })
     })
 
@@ -50,14 +101,51 @@ export default class Chat extends Component<IAppProps, IAppState> {
       this.peer.send('any' + Math.random())
     })
 
+
     this.peer.on('data', (data: any) => {
       console.log('data: ' + data)
       this.setState({ data: String(data) })
     })
+
+      (this.socket as WebSocket).onmessage((message: WebSocketMessage) => {
+        if (message.type === "answer") {
+          this.peer.signal(message.data)
+        }
+      })
   }
 
-  initConnection = () => this.createPeer(true)
-  joinConnection = () => this.createPeer(false)
+  addPeer = (userId: string, signal: any) => {
+    const peer = new SimplePeer({
+      initiator: false,
+      trickle: false,
+    })
+
+    peer.on('error', (err: Error) => console.log('error', err))
+
+    peer.on('signal', (data: any) => {
+      console.log('SIGNAL', data)
+      this.socket?.send(JSON.stringify({
+        type: 'answer',
+        data: {
+          from: userId,
+          data: data
+        }
+      }))
+      this.setState({ data: JSON.stringify(data, null, 2) })
+    })
+
+
+    peer.on('data', (data: any) => {
+      console.log('data: ' + data)
+      this.setState({ data: String(data) })
+    })
+
+    peer.signal(signal);
+    return peer;
+  }
+
+  initConnection = () => this.createPeer(this.context.user.id, this.state.targetUserId)
+  /*  joinConnection = () => this.createPeer(false) */
 
   onSubmit = (e: React.FormEvent) => {
     e.preventDefault()
@@ -78,7 +166,10 @@ export default class Chat extends Component<IAppProps, IAppState> {
           color="primary"
           variant="outlined"
           size="small"
-          value="hey"
+          label="target user"
+          name="targetUserId"
+          value={this.state.targetUserId}
+          onChange={this.onChange}
         />
         <header>
           <p>
@@ -88,7 +179,7 @@ export default class Chat extends Component<IAppProps, IAppState> {
           </p>
         </header>
         <button onClick={this.initConnection}>init connection</button>
-        <button onClick={this.joinConnection}>join connection</button>
+        {/* <button onClick={this.joinConnection}>join connection</button> */}
         <form onSubmit={this.onSubmit}>
           <textarea
             name="textInput"
@@ -109,7 +200,6 @@ export default class Chat extends Component<IAppProps, IAppState> {
             <pre>{this.state.data}</pre>
           </>
         )}
-        <Socket />
       </div>
     )
   }
--- server/src/socket/socket.ts
+++ server/src/socket/socket.ts
@@ -7,6 +7,11 @@ const handleUpgrade = require('./handleUpgrade');
 
 export const UserMap = new Map<string, any>()
 
+interface SocketMessage {
+    type: string
+    payload: any
+}
+
 module.exports = function initSocketServer(server: Http2Server) {
     const wss = new WebSocket.Server({
         noServer: true,
@@ -18,8 +23,24 @@ module.exports = function initSocketServer(server: Http2Server) {
         /* tell all users that are your friends and online that you are online  */
         console.log(UserMap)
         //ws.send(data);
-        ws.on('message', data => {
-            console.log(data + "_" + ws.user);
+        ws.on('message', (message: SocketMessage) => {
+            switch (message.type) {
+                case 'fffer':
+                    sendOffer(message.payload, ws);
+                    break;
+                case 'answer':
+                    answerOffer(message.payload, ws);
+                    break;
+                case 'iceCandidate':
+                    handleIceCandidate(message.payload, ws);
+                    break;
+                default:
+                    ws.send({
+                        type: 'message',
+                        data: 'server is listening but this is no valid message type :)'
+                    })
+
+            }
             ws.send(data);
             UserMap.forEach(client => {
                 if (client.readyState === WebSocket.OPEN) {
@@ -30,4 +51,62 @@ module.exports = function initSocketServer(server: Http2Server) {
     });
     server.on('upgrade', (request, socket, head) => handleUpgrade(request, socket, head, wss));
     return wss;
+}
+
+function sendOffer(payload, ws) {
+    // sends an webRTC offer to a target socket if it is in the UserMap
+    const targeSocket = UserMap.get(payload.target);
+    if (targeSocket) {
+        targeSocket.send({
+            type: 'offer',
+            data: payload
+        });
+        return ws.send({
+            type: 'sendOffserSucces',
+            data: payload
+        })
+    }
+    ws.send({
+        type: 'sendOfferFail',
+        data: payload
+    })
+}
+
+function answerOffer(payload, ws) {
+    // sends the answer to an webRTC offer to a target socket if it is in the UserMap
+    // not much is stopping you from calling this the wrong way but a connection will not be formed
+    // if there was no previous offer
+    const targeSocket = UserMap.get(payload.target);
+    if (targeSocket) {
+        targeSocket.send({
+            type: 'answer',
+            data: payload
+        });
+        return ws.send({
+            type: 'answerOffserSucces',
+            data: payload
+        })
+    }
+    ws.send({
+        type: 'answerOfferFail',
+        data: payload
+    })
+}
+
+function handleIceCandidate(payload, ws) {
+    const targetSocket = UserMap.get(payload.target);
+    if (targetSocket) {
+        targetSocket.send({
+            type: 'iceCandidate',
+            data: payload
+        });
+        return ws.send({
+            type: 'iceSucces',
+            data: payload
+        })
+    }
+    ws.send({
+        type: 'iceFail',
+        data: payload
+    })
 }
